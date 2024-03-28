0d61adaabd0f5dc1ad7fd733bd9d0db7b7649b1c
xiidot1303 <xii1303@inbox.ru>
2024-03-28 09:09:22 +0500

    add path in app2

--- app2/apps.py
+++ app2/apps.py
@@ -1,8 +1,10 @@
 from django.apps import AppConfig
+from django.conf import settings
+import os
 
 class app2(AppConfig):
     name = 'app2'
-
+    path = os.path.join(settings.BASE_DIR, 'app2')
     def ready(self):
         # from app.models import About, Rule
         # About.objects.get_or_create(pk=1)
